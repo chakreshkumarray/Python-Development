39125f6c57a278848250e29f6e7b8242d3492eb0
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-20 11:46:53 +0530

    Create 1_Variable.py

---
+++ 1. Python Tutorial/3. Variable Data Type Naming Rule/1_Variable.py
@@ -0,0 +1,20 @@
+x = 1
+print(x)
+print(type(x))
+
+name = "Chakresh"
+print(name)
+print(type(name))
+
+price = 999.9
+print(price)
+print(type(price))
+
+is_adult = False
+print(is_adult)
+print(type(is_adult))
+
+a = 12
+b = 13
+c = 14
+print(a,b,c)
