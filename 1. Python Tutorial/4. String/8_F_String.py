5ff448eb20e9285c1e9878025a5d84164b469fa5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-22 21:19:14 +0530

    Create 8_F_String.py

---
+++ 1. Python Tutorial/4. String/8_F_String.py
@@ -0,0 +1,13 @@
+city = "Sultanpur"
+temp = 75
+weather = f"The tempreature in {city} is {temp} degree"
+print(weather)
+
+a = 10
+b = 12
+print(f"The sum of {a} and {b} is {a+b}")
+
+name = "Chakresh"
+print(f"First character of {name} is {name[0]}")
+
+print(f"This is a curly brace:}}")
