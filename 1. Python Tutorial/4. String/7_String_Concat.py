09ed7858ae915f7d38267042a132b435e67bb0e1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-22 21:19:33 +0530

    Create 7_String_Concat.py

---
+++ 1. Python Tutorial/4. String/7_String_Concat.py
@@ -0,0 +1,18 @@
+a = "1"
+b = "1"
+print(a+b)
+
+first_name = "Chakresh"
+last_name= "Kumar"
+print(first_name+" "+last_name)
+
+age = 23
+message = "My age is: "+str(age)
+print(message)
+
+city = "Lucknow"
+temp = 75
+weather = "The tempreature in "+city+ " is "+str(temp)+ " degree now."
+print(weather)
+
+
