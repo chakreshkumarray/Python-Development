cbc1c9e5bb98c1008adb092da150a144e006fbcf
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-22 21:19:17 +0530

    Create 11_Equal.py

---
+++ 1. Python Tutorial/4. String/11_Equal.py
@@ -0,0 +1,20 @@
+msg = "Help!"
+res = len(msg) >= 5
+print(res)
+
+print("apple" == "apple")
+print("apple" == "Apple")
+print("apple" == "orange")
+print("apple" != "orange")
+print(1 < 2)
+print(11 > 2)
+print(15 <= 8)
+print(23 >= 9)
+print("a" > "b")
+print("apple" < "banana")
+print("apple" < "ape")
+print(ord("a")) # 97
+print(ord("A")) # 65
+print(ord("C"))
+print(ord("c"))
+print(chr(99))
