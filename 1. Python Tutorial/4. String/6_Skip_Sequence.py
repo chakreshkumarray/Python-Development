7f69f11a054d7adbd48c93c5f901f5e19dc8ea90
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-22 21:18:59 +0530

    Create 6_Skip_Sequence.py

---
+++ 1. Python Tutorial/4. String/6_Skip_Sequence.py
@@ -0,0 +1,25 @@
+# Skip Sequence
+message = "Dont't worry about errors."
+message = 'Don\'t worry about erroes.'
+print(message)
+
+dialogue = 'She said ,"Python is amazing!"'
+dialogue = "She said ,\"Python is amazing!\""
+print(dialogue)
+
+poem = '''wuebfjfhuehtheihvbdkvaeiufef,asnbvkadhguoe,anvjdahgke,avdnakgegnad,
+     gkfgkajng,dhguaehgjadngkjehghafgn,j gdabskbgkbgndgjksgkjeg,dngkjaekgnhqriuteqgrngrhgogjgiuhgehgsngjhiugyegbiohfkanv ,udhedklgheioyteytqetpoieutqeqehehgndlbsodjdvdvbaofejgeglkhgoeigdlkv 
+     zx,nvhxkgfywegebgbgjeggegsd,gnklhehiegebggoiyeegbkguie'''
+print(poem)
+
+story = "wuebfjfhuehtheihvbdkvaeiufef,asnbvkadhguoe,anvjdahgke,\navdnakgegnad,gkfgkajng,dhguaehgjadngkjehghafgn,j g\ndabskbgkbgndgjksgkjeg,dngkjaekgnhqriuteqgrngrhgogjgiuhgehgsngjhiugyegbiohfkanv ,udhedklgheioyteytqetpoie\nutqeqehehgndlbsodjdvdvbaofejgeglkh\ngoeigdlkv zx,nvhxkgfywegebgbgjeggegsdgn\nklhehiegebggoiyeegbkguie"
+print(story)
+
+directory = "C:\user\kumar\Downloads\photos"
+print(directory)
+
+print("HEL\P")
+print("HEL\U")
+print("HEL\nP")
+print("Hel\\lo")
+print("HEL\tP")
