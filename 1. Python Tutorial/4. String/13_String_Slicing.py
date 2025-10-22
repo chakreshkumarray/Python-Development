39d1d4bb89804e41460d8d60adaa49fb77425d05
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-22 21:18:40 +0530

    Create 13_String_Slicing.py

---
+++ 1. Python Tutorial/4. String/13_String_Slicing.py
@@ -0,0 +1,16 @@
+txt = "Chakresh Kumar"
+print(txt[0:1])
+print(txt[0:4])
+
+print(txt[:6])
+print(txt[9:])
+print(txt[:])
+
+print(txt[7:10])
+print(txt[-11:-8])
+
+print(txt[1:6:2])
+
+# traverse
+print(txt[::-2])
+print(txt[::-1])
