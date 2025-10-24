0c0d0f9d66172c57c7efa4fb8c65fc812851b0ce
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:13:00 +0530

    Create 4_Assignment_Operators.py

---
+++ 1. Python Tutorial/5. Numbers/4_Assignment_Operators.py
@@ -0,0 +1,31 @@
+a = 1
+b = 2
+
+a,b = 1,2
+
+c = 3
+d = 3
+
+c = (d == 3)
+
+'''
+# Swap Number Take input from user
+x = int(input("Enter value of x: "))
+y = int(input("Enter value of y: "))
+temp = x
+x = y
+y = temp
+
+#  Swap in Python
+# x,y = y,x
+
+print(x,y)
+'''
+
+z = 3
+z = z + 5
+print(z)
+
+salary = 11
+salary **= 2
+print(salary)
