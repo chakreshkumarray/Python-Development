387551854b852d980f2085f5623467af18c69c89
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:13:05 +0530

    Create 3_Bitwise_Operators.py

---
+++ 1. Python Tutorial/5. Numbers/3_Bitwise_Operators.py
@@ -0,0 +1,26 @@
+x = 10 # 1010
+y = 6 # 0110
+
+# AND &
+print(x & y)
+
+# OR |
+print(x | y)
+
+# XOR ^
+print(x ^ y)
+
+# Not ~
+print(~x)
+
+# Left Shift
+print(x >> 1)
+
+# Right Shift
+print(x >> 1)
+
+# question
+print(10 // 3)   # Ans 3
+print(10.0 // 3) # Ans 3.0
+print(-10 // 3)  # Ans -4
+print(10 // -3)  # Ans -4
