fc60e966fddb81b49a5a7147dd4b0c83305ba2f7
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:13:08 +0530

    Create 2_Arithmetic_Operators.py

---
+++ 1. Python Tutorial/5. Numbers/2_Arithmetic_Operators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 3
+print("Addition: ",a+b)
+print(f"Subtraction: {a-b}")
+print(f"Multiplication: {a*b}")
+print(f"Division: {a/b}")
+print(f"Modulus: {a%b}")
+print(f"Power: {a**b}")
+print(f"Negation: {a} = {-a}")
+print(f"Absolute: {abs(b-a)}")
+print(f"Regular division: {a//b}")
+
+result = 2 + 3 * 4
+print(result)
+
+question = 10 - ((2 ** 3) // 2) + 7
+print(question)
