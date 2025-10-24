20644ca37476b75dd2607384f4acf0fee33a195b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:12:52 +0530

    Create 6_Complex_Numbers.py

---
+++ 1. Python Tutorial/5. Numbers/6_Complex_Numbers.py
@@ -0,0 +1,17 @@
+z = 1 + 2j
+print(z)
+print(z.real)
+print(z.imag)
+
+z1 = 2 + 3j
+z2 = 1 - 2j
+
+sum_z = z1 + z2
+print(sum_z)
+diff_z = z1 - z2
+prod_z = z1 * z2
+quot_z = z1 / z2
+mag_z = abs(z1)
+
+z3 = complex(2,3)
+print(z3)
