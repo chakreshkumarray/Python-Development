23850f83c39b5ae446856f270e8b0ca7d905e4a5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:12:56 +0530

    Create 5_Floats.py

---
+++ 1. Python Tutorial/5. Numbers/5_Floats.py
@@ -0,0 +1,49 @@
+salary = 1.56
+print(salary)
+z = -0.323
+x = - .2332
+print(x)
+print(z)
+a = 4.0
+print(type(a))
+b = 7 / 2
+print(b)
+
+c = float(7)
+print(c)
+
+result = 0.1 + 0.2
+print(result)
+
+electron_mass = 9.11e-31
+print(electron_mass)
+
+number1 = 5e31 * 2e2
+print(number1)
+
+number = 5e3 * 2e2
+print(number)
+
+very_large = 10000000000000000000000.0
+print(very_large)
+
+pi = 3.14141704036532326
+print(f"{pi:.2f}")
+
+a = 0.3
+b = 0.1 + 0.2
+print(a)
+print(b)
+print(a == b)
+
+import math
+print(math.isclose(a,b))
+
+import decimal
+a = decimal.Decimal('0.1')
+b = decimal.Decimal('0.2')
+print(a + b)
+
+pi = 4.401641656
+print(pi,3)
+print(round(pi,3))
