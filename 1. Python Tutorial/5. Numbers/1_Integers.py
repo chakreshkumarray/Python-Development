c76b3d6caee5e1830cafe97107dae4f74f886997
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-24 20:13:13 +0530

    Create 1_Integers.py

---
+++ 1. Python Tutorial/5. Numbers/1_Integers.py
@@ -0,0 +1,25 @@
+a = 10
+b = -22
+c = 0
+print(a)
+print(b)
+print(c)
+print(type(a))
+
+# Python integers have unlimited precision
+i = 46447469895682552525
+print(i)
+print(type(i))
+
+salary = 10_00_000
+print(salary)
+
+binary_num = 55
+print(bin(binary_num))
+num = 0b101
+print(bin(num))
+print(type(num))
+print(num)
+
+hex_num = 0x2A
+print(hex_num)
