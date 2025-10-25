3446c03943338cfa9df3c00a1ff618726256baa6
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-25 06:46:30 +0530

    Create 1_Boolean.py

---
+++ 1. Python Tutorial/6. Boolean/1_Boolean.py
@@ -0,0 +1,16 @@
+a = True
+b = False
+print(type(a))
+
+age = 17
+isAdult = age >= 18
+print(isAdult)
+
+# bool is a Subclass of int
+print(True == 1)
+print(False == 0)
+
+print(True + True)
+print(True)
+print(int(True))
+print(int(False))
