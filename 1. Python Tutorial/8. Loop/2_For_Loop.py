5f9ace7253dc6fadf62656a438f8eb938d03899c
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-26 14:06:33 +0530

    Create 2_For_Loop.py

---
+++ 1. Python Tutorial/8. Loop/2_For_Loop.py
@@ -0,0 +1,17 @@
+for i in range(1,11,1):
+  print(i)
+
+# 
+for i in range(10, 0, -1):
+  print(i)  
+
+#
+fruits = ["Banana","Papaya","Orange","Mango"]
+for i in range(len(fruits)):
+  print(fruits[i])
+
+#
+fruits = ["Banana","Papaya","Orange","Mango"]
+for i in range(len(fruits) -1, -1, -1):
+  print(fruits[i])
+  
