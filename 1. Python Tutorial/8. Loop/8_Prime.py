4e09b911c784ebb694d6949985c2762bdcce7606
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-26 14:06:54 +0530

    Create 8_Prime.py

---
+++ 1. Python Tutorial/8. Loop/8_Prime.py
@@ -0,0 +1,12 @@
+
+# Check Prime Number
+
+x = int(input("Enter a number: "))
+count = 0
+for i in range(1, x + 1):
+  if x % i == 0:
+    count +=1
+if count == 2:
+  print("Prime")
+else:
+  print("Not Prime")      
