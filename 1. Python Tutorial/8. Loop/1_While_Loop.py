a2cca6b69c21a19e30a064e1bb2edda723db264f
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-26 14:06:45 +0530

    Create 1_While_Loop.py

---
+++ 1. Python Tutorial/8. Loop/1_While_Loop.py
@@ -0,0 +1,31 @@
+x = 1
+while x <= 10:
+  print("Ck")
+  x += 1
+
+# break
+x = 1
+while x <= 5:
+  print("Hello",x)
+  x += 1
+  if x == 4:
+    break
+
+x = 1
+while True:
+  print("OK")
+  x += 1
+  if x == 2:
+    break
+
+# Number count
+x = 1
+while x <= 10:
+  print("number: ",x)
+  x += 1
+
+# reverse count
+x = 10
+while x >= 0:
+  print(x)
+  x -= 1  
