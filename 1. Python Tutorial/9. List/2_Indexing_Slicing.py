d72cf4ac580dda76a6ababac6acc69588a4ba63e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-28 16:26:31 +0530

    Create 2_Indexing_Slicing.py

---
+++ 1. Python Tutorial/9. List/2_Indexing_Slicing.py
@@ -0,0 +1,23 @@
+fruits = ["Apple","Banana","Cherry"]
+print(type(fruits))
+print(fruits)
+# indexing
+print(fruits[0])
+
+numbers = [10,20,30,40,50]
+print(numbers)
+# slicing list[start:end:step]
+print(numbers[0:3])
+print(numbers[:3])
+print(numbers[:])
+print(numbers[1:])
+print(numbers[1::])
+print(numbers[1::1])
+print(numbers[1::2])
+print(numbers[1::22])
+print(numbers[len(numbers)-1])
+
+# Reverse the list
+print(numbers[::-1])
+
+print(numbers[-3:])
