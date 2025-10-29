9ce5ab3f0dd1b17065bb7f4583e9c8a7a5cdf9b0
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-29 16:06:38 +0530

    Update 3_Modifying_List.py

--- 1. Python Tutorial/9. List/3_Modifying_List.py
+++ 1. Python Tutorial/9. List/3_Modifying_List.py
@@ -1,1 +1,36 @@
-fruist = ["Apple","Banana","Cherry"]
+fruits = ["Apple","Banana","Cherry"]
+print(fruits)
+
+# Append
+fruits.append("Mango")
+print(fruits)
+
+# insert
+fruits.insert(1,"Papaya")
+print(fruits)
+
+# extend
+fruits1 = ["Grapes","PineApple"]
+fruits.extend(fruits1)
+print(fruits)
+
+# remove
+fruits.remove("Banana")
+print(fruits)
+
+# Pop
+fruits.pop()
+print(fruits)
+fruits.pop(2)
+print(fruits)
+
+# delete new method
+del fruits[1]
+print(fruits)
+
+del fruits[1:3]
+print(fruits)
+
+# Clear
+fruits.clear()
+print(fruits)
