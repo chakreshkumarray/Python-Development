c08a5a9f00b7e7e0a65ed7ec51cde6709dc2aeb1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-30 19:21:17 +0530

    Create 7_Reverse_Sorting.py

---
+++ 1. Python Tutorial/9. List/7_Reverse_Sorting.py
@@ -0,0 +1,17 @@
+nums = [1,2,3,4]
+print(nums)
+
+# Reverse
+nums.reverse()
+print(nums)
+
+# Sort
+words = ["Shivam","Chakresh","Ambreesh","Dheeraj"]
+words.sort()
+words.sort(reverse = True)
+words.sort
+print(words)
+sorted_words = sorted(words)
+print(sorted_words)
+
+# code start from here at 49:59
