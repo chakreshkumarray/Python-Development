12f459600093d8a487106b9209fb1e35a357fc70
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-10-30 19:21:12 +0530

    Update 6_Function_List.py

--- 1. Python Tutorial/9. List/6_Function_List.py
+++ 1. Python Tutorial/9. List/6_Function_List.py
@@ -1,1 +1,10 @@
 numbers = [10,5,8,3]
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+# 
+names = ["Alice","Bob","ALice","Charlie"]
+print(min(names))
+print(max(names))
+print(ord("l"),ord("L"))
