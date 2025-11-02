876059aed5b4a4f930b06bf91d723f717a886efd
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-02 20:20:15 +0530

    Update 7_Reverse_Sorting.py

--- 1. Python Tutorial/9. List/7_Reverse_Sorting.py
+++ 1. Python Tutorial/9. List/7_Reverse_Sorting.py
@@ -14,4 +14,22 @@ print(words)
 sorted_words = sorted(words)
 print(sorted_words)
 
+# sorted
+words = ["Shivam","Chakresh","Ambreesh","Dheeraj"]
+sorted_words = sorted(words, key=len, reverse=True)
+print(sorted_words)
+
+# abs
+nums = [-10,8,-9,-2,4,-6]
+print(nums)
+nums.sort()
+print(nums)
+nums.sort(key = abs)
+print(nums)
+
 # 
+words = ["Shivam","Chakresh","Ambreesh","Dheeraj"]
+print(sorted(words,key=str.lower,reverse=True))
+print(words[1:3].count("Shivam"))
+print(words.count("Shivam"))
+print(words.index("Shivam",0))
