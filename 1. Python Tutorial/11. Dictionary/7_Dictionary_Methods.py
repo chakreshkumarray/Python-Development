82233f1071af9eba1a07e380db3028fcb4590fe8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-04 17:32:33 +0530

    Create 7_Dictionary_Methods.py

---
+++ 1. Python Tutorial/11. Dictionary/7_Dictionary_Methods.py
@@ -0,0 +1,19 @@
+
+student = {
+  'name':"Alice",
+  'age':23,
+  'grade':"A"
+}
+
+# Keys methods
+print(student.keys())
+
+keys = student.keys()
+student['city'] = "New York" 
+print(keys)
+
+# values methods
+print(student.values())
+
+# items methods
+print(student.items())
