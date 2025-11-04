faf839800b2cb98c060ada1287df56e39795cafb
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-04 17:32:15 +0530

    Create 2_Creating_Dictionary.py

---
+++ 1. Python Tutorial/11. Dictionary/2_Creating_Dictionary.py
@@ -0,0 +1,36 @@
+'''
+Syntax :-
+dictionary_name = {
+ key1: value1
+ key2: value2
+ key3: value3
+}
+
+'''
+# Empty Dictionary
+d = {}
+
+fruits = {
+  "apple": "red",
+  "banana": "yellow",
+  "kiwi": "brown"
+}
+
+print("vic", end = " ..")
+print("vic", end = " ..")
+
+dict(apple = "red", banana = "yellow", kiwi = "brown")
+
+d = dict([('apple','red'),('banana','yellow'),('kiwi','brown')])
+print(d)
+
+int_key = {
+  1 : "One",
+  2 : "Two",
+  3 : "Three"
+}
+
+bool_Key = {
+  True: "It's true !",
+  False: "It's false !"
+}
