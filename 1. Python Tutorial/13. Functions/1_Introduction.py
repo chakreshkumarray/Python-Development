c9cd0abdd610ab72c713a177ffb450c65fdd9c03
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-09 12:44:37 +0530

    Create 1_Introduction.py

---
+++ 1. Python Tutorial/13. Functions/1_Introduction.py
@@ -0,0 +1,34 @@
+
+# print("Hello Chakresh")
+# len()
+
+# Function without Argument
+# def fun():
+#   print("Chakresh")
+#   print("Kumar")
+#   print("Ray")
+# fun()
+
+# # Function with Argument
+# def intro(first_name,last_name,country):                      # Parameter
+#   print("Hello world")
+#   print(f"My name is {first_name} {last_name}")
+#   print(f"i live in {country}")
+#   print("Bye !")
+# intro("Chakresh","Kumar","India")                          # Argument
+
+
+# # Function with No return value
+# def intro(first_name,last_name,country):                      
+#   print("Hello world")
+#   print(f"My name is {first_name} {last_name}")
+#   print(f"i live in {country}")
+#   print("Bye !")
+# res = intro("Chakresh","Kumar","India")
+# print(res) 
+
+
+# Add Two Number
+def add(a,b):
+  print(f"sum of {a} and {b} is {a+b}")
+add(12,13)  
