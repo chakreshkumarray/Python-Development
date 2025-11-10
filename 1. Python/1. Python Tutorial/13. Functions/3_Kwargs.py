b48e1da9b4609200cb2c607c3cf5a9c3a9000ba3
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-10 20:57:46 +0530

    Update 3_Kwargs.py

--- 1. Python/1. Python Tutorial/13. Functions/3_Kwargs.py
+++ 1. Python/1. Python Tutorial/13. Functions/3_Kwargs.py
@@ -1,5 +1,13 @@
 
 # **Kwargs
 
-def build_profile():
-  print("")
+def build_profile(**kwargs):
+  print("Building profile..")
+  for i, j in kwargs.items():
+    print(f"{i}: {j}")
+
+build_profile(name = "CK", age = 23, profession = "SD",hobbies = ["Cricket","Movies","Coding"] , city = "Sultanpur")
+
+# Combined 38:35
+def mixed_example():
+  print()
