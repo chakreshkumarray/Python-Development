d6e810f33af09dd61559a58807461b6dd6f75bd5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-10 20:49:16 +0530

    Update 2_Args.py

--- 1. Python/1. Python Tutorial/13. Functions/2_Args.py
+++ 1. Python/1. Python Tutorial/13. Functions/2_Args.py
@@ -1,1 +1,25 @@
 
+# sum of three number
+def sumofthree(a, b, c):
+  return a + b + c
+
+print(sumofthree(2,3,5))
+
+# * Args
+def sum_of_nums(*nums):
+  total = 0
+  for i in nums:
+    total = total + i
+  return total
+
+print(sum_of_nums(4,4,5,3,6,2,1))
+
+# * Args multiple type args
+def sum_of_nums(*nums, msg):
+  print(msg)
+  total = 0
+  for i in nums:
+    total = total + i
+  return total
+
+print(sum_of_nums(4,4,5,3,1,msg = "I am calculating message"))
