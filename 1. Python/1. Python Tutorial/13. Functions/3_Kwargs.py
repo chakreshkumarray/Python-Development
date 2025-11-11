8d2b412528d1c9b3315dd9ea7dcbececb32d24e5
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-11 08:48:42 +0530

    Update 3_Kwargs.py

--- 1. Python/1. Python Tutorial/13. Functions/3_Kwargs.py
+++ 1. Python/1. Python Tutorial/13. Functions/3_Kwargs.py
@@ -1,5 +1,5 @@
 
-# **Kwargs
+# **Kwargs Keyword argument
 
 def build_profile(**kwargs):
   print("Building profile..")
@@ -8,6 +8,30 @@ def build_profile(**kwargs):
 
 build_profile(name = "CK", age = 23, profession = "SD",hobbies = ["Cricket","Movies","Coding"] , city = "Sultanpur")
 
-# Combined 38:35
-def mixed_example():
-  print()
+# Combined
+def mixed_example(msg,*args,**kwargs):
+  print(msg)
+  print(args)
+  print(kwargs)
+mixed_example("Example",1,2,3, name = "CK",role = "SDE")  
+
+# default kwargs
+def mixed_example(msg1,msg2,msg3 = "Message3", *args, **kwargs):
+  print(msg1, msg2, msg3)
+  print(args)
+  print(kwargs)
+mixed_example("Example1","Example2",1,2,3, name = "CK",role = "SDE")
+
+# add number with args
+def add(x,y,z):
+  return x + y + z
+
+vals = [1, 2, 3]
+print(add(*vals))
+
+params = {
+  "x" : 10,
+  "y" : 20,
+  "z" : 30
+}
+print(add(**params))
