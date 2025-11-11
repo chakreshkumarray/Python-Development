a3857ab51ec335bc4af9a57e22228391c9593bb2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-11 20:41:44 +0530

    Create 2_Read_File_From_Another_Folder.py

---
+++ 1. Python/2. Project/1. Move Read Write Update File Between Folder/5. Read File/2_Read_File_From_Another_Folder.py
@@ -0,0 +1,24 @@
+
+import os
+
+def read_file():
+  try:
+    source_folder = r"E:\Python-Development\1. Python\2. Project\1. Move Read Write Update File Between Folder\1. Source File"
+    file_name = "read_file_from_another_folder.csv"
+
+    file_path = os.path.join(source_folder, file_name)
+
+    if not os.path.exists(file_path):
+      print("❌ File not found!")
+      return
+
+    with open(file_path, "r", encoding="utf-8") as file:
+      print("✅ File content:\n")
+      print(file.read())
+
+  except Exception as e:
+    print("⚠️ Error:", e)
+
+
+if __name__ == "__main__":
+    read_file()
