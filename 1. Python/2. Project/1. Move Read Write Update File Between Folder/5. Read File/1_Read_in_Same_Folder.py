0f87dec2e1b94434f95197a11477406ab5814bc8
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-11 20:41:40 +0530

    Create 1_Read_in_Same_Folder.py

---
+++ 1. Python/2. Project/1. Move Read Write Update File Between Folder/5. Read File/1_Read_in_Same_Folder.py
@@ -0,0 +1,12 @@
+
+# Open the file in read mode
+file = open("Read_file_in_folder.csv", "r")
+
+# Read the entire content
+content = file.read()
+
+# Print the content
+print(content)
+
+# Close the file
+file.close()
