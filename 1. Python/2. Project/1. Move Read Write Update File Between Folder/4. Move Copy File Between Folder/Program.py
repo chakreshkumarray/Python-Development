4699b29e2cd97cbc677ffae8896c644abfdb78a1
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-12 13:31:08 +0530

    Update Program.py

--- 1. Python/2. Project/1. Move Read Write Update File Between Folder/4. Move Copy File Between Folder/Program.py
+++ 1. Python/2. Project/1. Move Read Write Update File Between Folder/4. Move Copy File Between Folder/Program.py
@@ -2,30 +2,24 @@
 import os
 import shutil
 
+# --- Define paths ---
 source_folder = r"E:\Python-Development\1. Python\2. Project\1. Move Read Write Update File Between Folder\1. Source File"
 destination_folder = r"E:\Python-Development\1. Python\2. Project\1. Move Read Write Update File Between Folder\2. Destination File"
-file_name = "students_data_marks.csv"
+file_name = "move_copy_of_file.csv"
 
-# build full path
+# Join the full path
 source_path = os.path.join(source_folder, file_name)
 destination_path = os.path.join(destination_folder, file_name)
 
-# Check multiple conditions
-if not os.path.exists(source_path):
-  print("❌ Source file not found!")
-
-elif not os.path.isdir(source_folder):
-  print("❌ Source folder does not exist!")
-
-elif not os.path.isdir(destination_folder):
-  print("⚠️ Destination folder not found! Creating it...")
-  os.makedirs(destination_folder, exist_ok=True)
-  shutil.copy(source_path, destination_path)
-  print(f"{file_name} ✅ File copied successfully after creating destination folder!")
-
-elif os.path.exists(destination_path):
-  print("⚠️ File already exists in destination folder!")
-
+# Send copy of file
+if not os.path.isfile(source_path):
+    print("❌ Source file not found!")
 else:
-  shutil.copy(source_path, destination_path)
-  print(f"{file_name} ✅ File copied successfully!")    
+  # Create destination folder if missing
+  os.makedirs(destination_folder, exist_ok=True)
+
+  if os.path.exists(destination_path):
+    print("⚠️ File already exists in destination folder!")
+  else:
+    shutil.copy(source_path, destination_path)
+    print(f"✅ '{file_name}' copied successfully to '{destination_folder}'.")
