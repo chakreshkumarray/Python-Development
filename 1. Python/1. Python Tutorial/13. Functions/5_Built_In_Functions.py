45618016662584e6e42b195f906cf98e66fe25bd
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-11-16 18:56:08 +0530

    Create 5_Built_In_Functions.py

---
+++ 1. Python/1. Python Tutorial/13. Functions/5_Built_In_Functions.py
@@ -0,0 +1,24 @@
+
+# Type converstion
+number_str = "42"
+number_int = int(number_str)
+print(type(number_int))
+
+price = float("19.99")
+age = 25
+age_str = str(age)
+
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("hi"))
+
+numbers = (1, 2, 3, 4)
+numbers_list = list(numbers)
+numbers_set = set(numbers)
+print(numbers_list)
+print(numbers_set)
+
+# Mathematical functions 1:07:40
+# abs
+print(abs(-5))
